b9a839572183ec972964626283dec507bf8e0b0c
Ilya Zverev <zverik@textual.ru>
2022-04-01 21:17:04 +0300

    Fix certificate error and icon for Android 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:every_door/constants.dart';
 import 'package:every_door/screens/loading.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_dropdown_alert/dropdown_alert.dart';
 
 void main() {
+  installCertificate();
   runApp(ProviderScope(child: const EveryDoorApp()));
+}
+
+installCertificate() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ByteData data = await PlatformAssetBundle().load('assets/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
 }
 
 class EveryDoorApp extends StatelessWidget {
