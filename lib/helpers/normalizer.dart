6c2135ef29581380ad577bb025a033e9c166bbdf
Revtro <118647831+Lonerat@users.noreply.github.com>
2023-01-04 16:19:05 +0900

    Update normalizer.dart

--- lib/helpers/normalizer.dart
+++ lib/helpers/normalizer.dart
@@ -1,6 +1,6 @@
 import "package:unorm_dart/unorm_dart.dart" as unorm;
 
 String normalizeString(String s) {
-  var combining = RegExp(r"[\u0300-\u036F\u3099\u309A\u309B\u309C]");
+  var combining = RegExp(r"[\u0300-\u036F\u3099-\u309C]");
   return unorm.nfkd(s.toLowerCase().trim()).replaceAll(combining, '');
 }
