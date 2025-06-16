89195a65c1e21fff911c64aab00d5c7be9bb437e
Ilya Zverev <zverik@textual.ru>
2025-06-16 09:54:44 +0200

    ios build

---
+++ ios/Share Extension/ShareViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.swift
+//  Share Extension
+//
+//  Created by user937662 on 6/12/25.
+//
+
+import UIKit
+import Social
+import listen_sharing_intent
+
+class ShareViewController: RSIShareViewController {
+
+}
